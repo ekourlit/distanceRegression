c83b221fefd6fb15e0386ac87c2e9b48f5cdbb25
Evangelos Kourlitis <evangelos.kourlitis@cern.ch>
2020-05-08 16:25:11 -0500

    full implemented and working data API pipeline

--- VangelisCode/3DCubeRegression.py
+++ VangelisCode/3DCubeRegression.py
@@ -13,41 +13,13 @@ from tensorflow.keras.optimizers import *
 timestamp = str(datetime.now().year)+str("{:02d}".format(datetime.now().month))+str("{:02d}".format(datetime.now().day))+str("{:02d}".format(datetime.now().hour))+str("{:02d}".format(datetime.now().minute))
 today = str(date.today())
 
-# tf.data API WIP
-# that's a great function but it still experimental and doesn't work with AutoGraph (not convertible)
 def consumeCSV(path):
 	'''
 	create tf.data.dataset by mapping on a tf.data.dataset containing file paths
+	that's a great function but it still experimental and doesn't work with AutoGraph (not convertible)
 	'''
 	scv_columns = ['X','Y','Z','Xprime','Yprime','Zprime','L']
 	return tf.data.experimental.make_csv_dataset(path, batch_size=1, column_names=scv_columns, label_name=scv_columns[-1], num_epochs=1)
-
-# tf.data API WIP
-def consumePickle(path):
-	'''
-	similar to consumeCSV
-	'''
-	file = open(path.numpy(),'rb')
-	data_dict = pickle.load(file)
-	columns = len(data_dict.keys())
-	firstColumn = next(iter(data_dict))
-	entries = len(data_dict[firstColumn])
-	dataset = tf.data.Dataset.from_tensor_slices(data_dict)
-	return dataset, entries, columns
-
-# tf.data API WIP
-def tf_consumePickle(path):
-	'''
-	the tf function that feed into the map
-	'''
-	# path_shape = path.shape
-	# [path,] = tf.py_function(func=consumePickle, inp=[path], Tout=[tf.float32])
-	# path.set_shape(path_shape)
-	# return path
-	dataset, entries, columns = tf.py_function(func=consumePickle, inp=[path], Tout=[tf.float32, tf.int32, tf.int32])
-	for example in dataset: break
-	pdb.set_trace()
-	return dataset
 
 def process_csv_line(line):
 	'''
@@ -57,76 +29,75 @@ def process_csv_line(line):
 	# parse csv line
 	fields = tf.io.decode_csv(line, [tf.constant([np.nan], dtype=tf.float32)] * 7)
 	# first 3 fields are X,Y,Z. normalize them
-	position = tf.stack(fields[:3])/0.5
+	position = tf.stack(fields[:3])/positionNormalisation
 	# next 3 fields are Xprime,Yprime,Zprime
 	momentum = tf.stack(fields[3:-1])
 	# stack and flatten them
 	features = tf.reshape(tf.stack([position,momentum]), [-1])
 	# last field is the length. normalize it.
-	length = tf.stack(fields[-1:])/normalisation
+	length = tf.stack(fields[-1:])/lengthNormalisation
 	return features, length
 
-def getG4Datasets(G4FilePath, batch_size=32):
+def getG4Datasets(G4FilePath, batch_size=32, shuffle_buffer_size=1000, dataAPI=False):
 	'''
 	Construct the test datasets generated from Geant4.
 	arguments
 		HDF5File: the data file
 	return
-		test_data_input, test_data_output: 1 x tf.Tensor. Input shape (i,6), output shape (i,1).
+		data_input, data_output: 1 x tf.Tensor. Input shape (i,6), output shape (i,1).
 	'''
 	
 	if '.hdf5' in G4FilePath:
 		file = h5py.File(G4FilePath,'r')
 		
-		X = np.array(file['default_ntuples']['B4']['x']['pages'])/0.5 # convert these to -1 - 1 range
-		Y = np.array(file['default_ntuples']['B4']['y']['pages'])/0.5 # convert these to -1 - 1 range
-		Z = np.array(file['default_ntuples']['B4']['z']['pages'])/0.5 # convert these to -1 - 1 range
+		X = np.array(file['default_ntuples']['B4']['x']['pages'])/positionNormalisation # convert these to -1 - 1 range
+		Y = np.array(file['default_ntuples']['B4']['y']['pages'])/positionNormalisation # convert these to -1 - 1 range
+		Z = np.array(file['default_ntuples']['B4']['z']['pages'])/positionNormalisation # convert these to -1 - 1 range
 		Xprime = np.array(file['default_ntuples']['B4']['dx']['pages'])
 		Yprime = np.array(file['default_ntuples']['B4']['dy']['pages'])
 		Zprime = np.array(file['default_ntuples']['B4']['dz']['pages'])
 		L = np.array(file['default_ntuples']['B4']['distance']['pages'])
-		assert (np.any(L>normalisation)==False), "There are too large lengths in your dataset!"
+		assert (np.any(L>lengthNormalisation)==False), "There are too large lengths in your dataset!"
 
-		test_data_input = tf.convert_to_tensor(np.column_stack((X,Y,Z,Xprime,Yprime,Zprime)))
+		data_input = tf.convert_to_tensor(np.column_stack((X,Y,Z,Xprime,Yprime,Zprime)))
 		# a normalisation of the output is also happening
-		test_data_output = tf.convert_to_tensor(np.column_stack(L/normalisation).T)
+		data_output = tf.convert_to_tensor(np.column_stack(L/lengthNormalisation).T)
 
-		return test_data_input, test_data_output
+		return data_input, data_output
 
 	# csv input
 	else:
 
-		''' turn off for the moment to feed into data API infrastructure
-		data = np.loadtxt(G4FilePath, delimiter=',')
-
-		L = (data[:,6]/normalisation).reshape(data[:,6].size,1)
-		assert (np.any(L>1)==False), "There are too large lengths in your dataset!"
-		# convert X, Y, Z from -0.5 - 0.5 to -1 - 1 and concatenate with X', Y', Z'
-		inputs = np.concatenate((data[:,:3]/0.5, data[:,3:6]), axis=1)
-		
-		test_data_input = tf.convert_to_tensor(inputs)
-		test_data_output = tf.convert_to_tensor(L)
-
-		return test_data_input, test_data_output
-		'''
-
-		# using tf.data API WIP
-		if '*' in G4FilePath:
+		# using tf.data API
+		if dataAPI:
 			# create a file list dataset
 			file_list = tf.data.Dataset.list_files(G4FilePath)
 			# create TextLineDatasets (lines) from the above list
 			dataset = file_list.interleave(
-				lambda path: tf.data.TextLineDataset(path),
-				cycle_length=1,
-				block_length=1)
+				lambda path: tf.data.TextLineDataset(path).skip(15), #skip the first 15 lines as it's header
+				cycle_length=1) # the number of paths it concurrently process from file_list
 			# parse & process csv line
 			dataset = dataset.map(process_csv_line)
-			dataset = dataset.batch(batch_size)
+			# keep a hand in memory and shuffle
+			dataset = dataset.shuffle(shuffle_buffer_size)
+			# chop in batches and prepare in CPU 1 bach ahead before you feed into evaluation
+			dataset = dataset.batch(batch_size).prefetch(1)
 
-			test_data_input = dataset.map(lambda X, Y: X)
-			test_data_output = dataset.map(lambda X, Y: Y)
+			return dataset
+
+		# just load everything in memory
+		else:
+			data = np.loadtxt(G4FilePath, delimiter=',', skiprows=15)
+
+			L = (data[:,6]/lengthNormalisation).reshape(data[:,6].size,1)
+			assert (np.any(L>1)==False), "There are too large lengths in your dataset!"
+			# normalise X, Y, Z and concatenate with X', Y', Z'
+			inputs = np.concatenate((data[:,:3]/positionNormalisation, data[:,3:6]), axis=1)
 			
-			return test_data_input, test_data_output
+			data_input = tf.convert_to_tensor(inputs)
+			data_output = tf.convert_to_tensor(L)
+
+			return data_input, data_output
 
 def getDatasets(pickleFile, validation_ratio=0.2):
 	'''
@@ -154,27 +125,23 @@ def getDatasets(pickleFile, validation_ratio=0.2):
 	train_data_input = tf.convert_to_tensor(train_data[['X','Y','Z','Xprime','Yprime','Zprime']].values)
 	validation_data_input = tf.convert_to_tensor(validation_data[['X','Y','Z','Xprime','Yprime','Zprime']].values)
 	# a normalisation of the output is also happening
-	train_data_output = tf.convert_to_tensor(train_data[['L']].values/normalisation)
-	validation_data_output = tf.convert_to_tensor(validation_data[['L']].values/normalisation)
+	train_data_output = tf.convert_to_tensor(train_data[['L']].values/lengthNormalisation)
+	validation_data_output = tf.convert_to_tensor(validation_data[['L']].values/lengthNormalisation)
 
 	return train_data_input, train_data_output, validation_data_input, validation_data_output
 
-def getMLP(inputShape, hiddenNodes, fActivation='relu', fOptimizer='adam', fOutputActivation='sigmoid', fLoss='mse'):
+def getMLP(fhiddenNodes, inputShape=6, fActivation='relu', fOptimizer='adam', fOutputActivation='sigmoid', fLoss='mse'):
 	'''
 	Construct a fully connected MLP.
-	arguments
-		hiddenNodes: list of int. len(hiddenNodes) specifies the number of hiddel layers
-		fActivation: activation function string
-		fOptimizer: optimizer string
 	return
 		model: keras model
 	'''
 	
 	# construct
 	model = tf.keras.Sequential()
-	model.add(tf.keras.layers.Dense(hiddenNodes[0], input_shape=(inputShape,), activation=fActivation))
-	for layer in range(1, len(hiddenNodes)):
-		model.add(tf.keras.layers.Dense(hiddenNodes[layer], activation=fActivation))
+	model.add(tf.keras.layers.Dense(fhiddenNodes[0], input_shape=(inputShape,), activation=fActivation))
+	for layer in range(1, len(fhiddenNodes)):
+		model.add(tf.keras.layers.Dense(fhiddenNodes[layer], activation=fActivation))
 	model.add(tf.keras.layers.Dense(1, activation=fOutputActivation))
 	# check kernel_initializer='he_uniform'. it doesn't learn.
 
@@ -191,7 +158,7 @@ def logConfiguration(dictionary):
 	f.write( str(dictionary) )
 	f.close()
 
-def perfPlots(prediction, truth, details, savename='results'):
+def perfPlots(prediction, truth, savename='results'):
 	'''
 	Produce a set of basic sanity/validation plots
 	arguments
@@ -209,28 +176,24 @@ def perfPlots(prediction, truth, details, savename='results'):
 	fig.tight_layout(pad=1.3)
 
 	# truth length
-	truth_length = truth*normalisation
-	axs[0].hist(truth_length, bins=100, range=(0,2))
+	truth_length = truth*lengthNormalisation
+	axs[0].hist(truth_length, bins=100)
 	axs[0].set(xlabel='Truth L', ylabel='Points')
 	
 	# predicted length
-	pred_length = prediction*normalisation
-	axs[1].hist(pred_length, bins=100, range=(0,2))
+	pred_length = prediction*lengthNormalisation
+	axs[1].hist(pred_length, bins=100)
 	axs[1].set(xlabel='Predicted L')
 
 	# error
 	# error = np.divide(truth_length - pred_length, truth_length, out=np.zeros_like(truth_length - pred_length), where=truth_length!=0)
 	error = truth_length - pred_length
 	abs_error = abs(error)
-	axs[2].hist(error, bins=100, log=True, range=(-1,1))
+	axs[2].hist(error, bins=100, log=True)
 	axis = plt.gca()
 	axis.minorticks_on()
 	# axs[2].set(xlabel='Truth L - Predicted L / Truth L')
 	axs[2].set(xlabel='Truth L - Predicted L')
-
-	# NN details
-	# fig.suptitle(printTable(details), size='xx-small')
-	# plt.text(0.6, 0.8, printTable(details), transform = axs[2].transAxes, size=5, bbox=dict(facecolor='white'))
 
 	# save
 	plt.tight_layout()
@@ -273,7 +236,7 @@ def perfPlots(prediction, truth, details, savename='results'):
 
 	# hist2d Error vs Truth
 	plt.clf()
-	plt.hist2d(truth_length.reshape(len(truth_length),), error.reshape(len(error),), bins=(50,50), range=[[0, 2], [-0.5, 0.5]],  norm=mpl.colors.LogNorm())
+	plt.hist2d(truth_length.reshape(len(truth_length),), error.reshape(len(error),), bins=(50,50),  norm=mpl.colors.LogNorm())
 	axis = plt.gca()
 	axis.set_xlabel('Truth L')
 	axis.set_ylabel('Truth L - Predicted L')
@@ -300,14 +263,14 @@ def combPerfPlots(Vprediction, Vtruth, Tprediction=None, Ttruth=None, savename='
 	else: combined = False
 
 	# validation
-	val_truth_length = Vtruth*normalisation
-	val_pred_length = Vprediction*normalisation
+	val_truth_length = Vtruth*lengthNormalisation
+	val_pred_length = Vprediction*lengthNormalisation
 	val_error = np.divide(val_truth_length - val_pred_length, val_truth_length, out=np.zeros_like(val_truth_length - val_pred_length), where=val_truth_length!=0)
 	val_abs_error = abs(val_error)
 	
 	if combined:
-		test_truth_length = Ttruth*normalisation
-		test_pred_length = Tprediction*normalisation
+		test_truth_length = Ttruth*lengthNormalisation
+		test_pred_length = Tprediction*lengthNormalisation
 		test_error = np.divide(test_truth_length - test_pred_length, test_truth_length, out=np.zeros_like(test_truth_length - test_pred_length), where=test_truth_length!=0)
 		test_abs_error = abs(test_error)
 
@@ -388,8 +351,9 @@ mlflow.keras.autolog()
 
 # define the input arguments
 parser = argparse.ArgumentParser(description='Regress distance to the boundary of a unit cube from 3D points and directions.')
-parser.add_argument('--myDataset', help='Pickle file of the dataset.', required=True)
-parser.add_argument('--G4Dataset', help='Import Geant4 generated dataset (HDF5 or CSV format).', default=None)
+parser.add_argument('--trainData', help='Train dataset.', required=False)
+parser.add_argument('--validationData', help='Train dataset.', required=True)
+parser.add_argument('--testData', help='Test dataset.', required=False)
 parser.add_argument('--plots', help='Produce sanity plots.', default=False, action='store_true')
 parser.add_argument('--model', help='Use a previously trained and saved MLP model.', default=None)
 parser.add_argument('--test', help='Model testing environment. Do not save.', default=False, action='store_true')
@@ -399,14 +363,10 @@ if __name__ == '__main__':
 	# parse the arguments
 	args = parser.parse_args()
 
-	# set cube length normalisation.
-	normalisation = math.sqrt(3)
+	# set normalisations
+	lengthNormalisation = 5543
+	positionNormalisation = 1600
 	
-	# get input/output train and validation data
-	trainX, trainY, valX, valY = getDatasets(args.myDataset, validation_ratio=0.1)
-	# validation_split: keep a small portion of the training data blind just to calculate performance during training.
-	validation_split = 0.05
-
 	# define your settings
 	settings = {
 		'Structure'				:	[1024,1024],
@@ -415,14 +375,17 @@ if __name__ == '__main__':
 		'OutputActivation'		:	'sigmoid',
 		'Loss'					:	'mse',
 		'Batch'					:	64,
-		'Epochs'				:	50,
-		'Training Sample Size'	:	int(trainX.shape[0]*(1-validation_split))
+		'Epochs'				:	2
+		# 'Training Sample Size'	:	int(trainX.shape[0]*(1-validation_split))
 	}
+
+	# get some train/validation data
+	if args.model is None: trainData = getG4Datasets(args.trainData, batch_size=settings['Batch'], dataAPI=True)
+	validationData = getG4Datasets(args.validationData, batch_size=settings['Batch'], shuffle_buffer_size=1, dataAPI=True)
 
 	# create MLP model
 	if args.model is None:
-		mlp_model = getMLP(inputShape=trainX.shape[1],
-						   hiddenNodes=settings['Structure'],
+		mlp_model = getMLP(fhiddenNodes=settings['Structure'],
 						   fActivation=settings['Activation'],
 						   fOptimizer=settings['Optimizer'],
 						   fOutputActivation=settings['OutputActivation'],
@@ -431,7 +394,11 @@ if __name__ == '__main__':
 		# print model summary
 		mlp_model.summary()
 		# fit model
-		history = mlp_model.fit(trainX, trainY, epochs=settings['Epochs'], batch_size=settings['Batch'], validation_split=validation_split)
+		history = mlp_model.fit(trainData,
+			                    epochs=settings['Epochs'],
+			                    validation_data=validationData)
+			                    # batch_size=settings['Batch'],
+			                    # validation_split=validation_split) # not supported with data API pipeline but you could feed dataset
 
 		# save model and print learning rate
 		if not args.test: 
@@ -445,39 +412,39 @@ if __name__ == '__main__':
 			plt.legend(['Train', 'Validation'], loc='upper right')
 			system('mkdir -p plots/'+today+'/'+timestamp)
 			plt.savefig('plots/'+today+'/'+timestamp+'/learning_'+timestamp+'.pdf')
+			print("Trained model saved! Timestamp:", timestamp)
 
 	# load MLP model
 	else:
 		mlp_model = tf.keras.models.load_model(args.model)
 		# print model summary
 		mlp_model.summary()
-		for item in args.model.split('/'):
-			if 'mlp' in item: timestamp = item.split('_')[-1]
+		timestamp = [item.split('_')[-1] for item in args.model.split('/') if 'mlp' in item][0]
 
 	# predict on validation data
-	print("Calculating predictions for %i points..." % len(valX))
+	print("Loading all validation data in memory...")
+	valX, valY = getG4Datasets(args.validationData)
+	print("Calculating validation predictions for %i points..." % len(valX))
 	pred_valY = mlp_model.predict(valX)
 
-	# prepare G4 dataset
-	if args.G4Dataset is not None:
-		g4X, g4Y = getG4Datasets(args.G4Dataset, batch_size=1024)
-		pdb.set_trace()
-
-		# predict on G4 data
-		# print("Calculating predictions for %i points..." % len(g4X))
-		pred_g4Y = mlp_model.predict(g4X)
-		pdb.set_trace()
+	# that would be the test dataset
+	if args.testData is not None:
+		print("Loading all test data in memory...")
+		testX, testY = getG4Datasets(args.testData)
+		# predict on test data
+		print("Calculating test predictions for %i points..." % len(testX))
+		pred_testX = mlp_model.predict(testX)
 
 	# plot
 	if args.plots: 
-		perfPlots(prediction=pred_valY, truth=valY.numpy(), details=settings, savename='resultsVal')
+		perfPlots(prediction=pred_valY, truth=valY.numpy(), savename='resultsVal')
 		inputPlots(valX.numpy(), savename='inputsVal')
-		if args.G4Dataset is not None: 
-			perfPlots(prediction=pred_g4Y, truth=g4Y.numpy(), details=settings, savename='resultsG4')
+		if args.testData is not None: 
+			perfPlots(prediction=pred_testX, truth=testY.numpy(), savename='resultsTest')
 			inputPlots(g4X.numpy(), savename='inputsG4')
 		
 		# combined plots
-		if args.G4Dataset is not None: combPerfPlots(Vprediction=pred_valY, Vtruth=valY.numpy(), Tprediction=pred_g4Y, Ttruth=g4Y.numpy())
+		if args.testData is not None: combPerfPlots(Vprediction=pred_valY, Vtruth=valY.numpy(), Tprediction=pred_testX, Ttruth=testY.numpy())
 		else: combPerfPlots(Vprediction=pred_valY, Vtruth=valY.numpy())		
 	
 	print("Done!")
